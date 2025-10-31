2d6efd902bad8023102aabb79254371fa1716a5c
tkhwang <tkhwang@gmail.com>
2025-10-31 13:45:59 +0900

    refactor(config): update PostgreSQL environment variables in docker-compose and CoreModule for consistency

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -22,11 +22,11 @@ export class CoreModule {
               useFactory: (configService: ConfigService) => {
                 return {
                   type: 'postgres',
-                  host: configService.get<string>('DB_HOST'),
-                  port: parseInt(configService.get<string>('DB_PORT') ?? '5432', 10),
-                  username: configService.get<string>('DB_USERNAME'),
-                  password: configService.get<string>('DB_PASSWORD'),
-                  database: configService.get<string>('DB_NAME'),
+                  host: configService.get<string>('DB_POSTGRE_HOST'),
+                  port: parseInt(configService.get<string>('DB_POSTGRE_PORT') ?? '5432', 10),
+                  username: configService.get<string>('DB_POSTGRE_USERNAME'),
+                  password: configService.get<string>('DB_POSTGRE_PASSWORD'),
+                  database: configService.get<string>('DB_POSTGRE_NAME'),
                 };
               },
             }),
