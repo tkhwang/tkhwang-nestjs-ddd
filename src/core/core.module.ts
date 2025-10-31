e93699198a12ca044332364510b317f2815e3713
tkhwang <tkhwang@gmail.com>
2025-10-31 13:24:47 +0900

    feat(core): enhance CoreModule with dynamic database configuration and logging

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,20 +1,34 @@
 import { ApplicationBootstrapOptions } from '@/common/application-bootstrap-options.interface';
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env.local', '.env'] })],
+})
 export class CoreModule {
   static forRoot(options: ApplicationBootstrapOptions) {
+    const logger = new Logger(CoreModule.name);
+    const driver = options.driver === 'orm' ? 'orm' : 'in-memory';
+    logger.log(
+      `CoreModule bootstrapping with ${driver === 'orm' ? '{orm}' : '{in-memory}'} provider.`,
+    );
+
     const imports =
       options.driver === 'orm'
         ? [
-            TypeOrmModule.forRoot({
-              type: 'postgres',
-              host: process.env.DB_HOST,
-              port: parseInt(process.env.DB_PORT ?? '5432'),
-              username: process.env.DB_USERNAME,
-              password: process.env.DB_PASSWORD,
-              database: process.env.DB_NAME,
+            TypeOrmModule.forRootAsync({
+              inject: [ConfigService],
+              useFactory: (configService: ConfigService) => {
+                return {
+                  type: 'postgres',
+                  host: configService.get<string>('DB_HOST'),
+                  port: parseInt(configService.get<string>('DB_PORT') ?? '5432', 10),
+                  username: configService.get<string>('DB_USERNAME'),
+                  password: configService.get<string>('DB_PASSWORD'),
+                  database: configService.get<string>('DB_NAME'),
+                };
+              },
             }),
           ]
         : [];
--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule.register({ driver: 'in-memory' }));
   await app.listen(process.env.PORT ?? 3000);
 }
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
