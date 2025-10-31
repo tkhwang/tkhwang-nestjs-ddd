91ba0100e1d9c038c2c077ef1d969e17ee247462
tkhwang <tkhwang@gmail.com>
2025-10-31 13:54:19 +0900

    docs(main): add note for configuring DB_DRIVER environment variable in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from '@/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(
+    // NOTE: To configure orm | in-memory, set DB_DRIVER environment variable in .env file
     AppModule.register({ driver: process.env.DB_DRIVER === 'orm' ? 'orm' : 'in-memory' }),
   );
   await app.listen(process.env.PORT ?? 3000);
