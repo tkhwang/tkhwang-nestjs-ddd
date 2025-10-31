aa97e9496f3b4ba372c7c37e31f09dbde5f31629
tkhwang <tkhwang@gmail.com>
2025-10-31 12:24:11 +0900

    feat(alarms): implement ORM persistence layer for alarms with repository and entity definitions

---
+++ src/alarms/infrastructure/persistence/orm/repositories/alarm.repository.ts
@@ -0,0 +1,24 @@
+import { AlarmRepository } from '@/alarms/application/ports/alarm.repository';
+import { Alarm } from '@/alarms/domain/alarm';
+import { AlarmEntity } from '@/alarms/infrastructure/persistence/orm/entities/alarm.entity';
+import { AlarmMapper } from '@/alarms/infrastructure/persistence/orm/mappers/alarm.mappter';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+export class OrmAlarmRepository implements AlarmRepository {
+  constructor(
+    @InjectRepository(AlarmEntity)
+    private readonly alarmRepository: Repository<AlarmEntity>,
+  ) {}
+
+  async findAll(): Promise<Alarm[]> {
+    const entities = await this.alarmRepository.find();
+    return entities.map((entity) => AlarmMapper.toDomain(entity));
+  }
+
+  async save(alarm: Alarm): Promise<Alarm> {
+    const persistenceAlarm = AlarmMapper.toPersistence(alarm);
+    const newEntity = await this.alarmRepository.save(persistenceAlarm);
+    return AlarmMapper.toDomain(newEntity);
+  }
+}
