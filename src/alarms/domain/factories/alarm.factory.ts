94c1fe3559c5bbbbd5f4d9dafe2210d292e50b2f
tkhwang <tkhwang@gmail.com>
2025-10-31 11:19:35 +0900

    fix(alarms): remove unnecessary type assertion in AlarmSeverity instantiation

--- src/alarms/domain/factories/alarm.factory.ts
+++ src/alarms/domain/factories/alarm.factory.ts
@@ -5,7 +5,7 @@ import { AlarmSeverity } from 'src/alarms/domain/value-objects/alarm-severity';
 export class AlarmFactory {
   create(name: string, severity: string) {
     const alarmId = randomUUID();
-    const alarmSeverity = new AlarmSeverity(severity as AlarmSeverity['value']);
+    const alarmSeverity = new AlarmSeverity(severity);
 
     return new Alarm(alarmId, name, alarmSeverity);
   }
